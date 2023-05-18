5140be81bf40636e6230152439fa1d879876abaf
nuhwebdev <131599177+nuhwebdev@users.noreply.github.com>
2023-05-18 19:40:59 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+let popup = document.getElementById("popup")
+
+function openPopUp(){
+    popup.classList.add("popup--open")
+}
+
+function closePopUp(){
+    popup.classList.remove("popup--open")
+}
