4b4b3c99149d10e6a7a95c69f3126210dbb019bf
nuhwebdev <131599177+nuhwebdev@users.noreply.github.com>
2023-05-26 23:26:31 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 let popup = document.getElementById("popup")
+let mobileMenuBurger = document.getElementById("mobileMenuBurger")
+let mobileMenu = document.getElementById("mobileMenu")
+let closeMobileMenu = document.getElementById("closeMobileMenu")
 
 function openPopUp(){
     popup.classList.add("popup--open")
@@ -7,3 +10,15 @@ function openPopUp(){
 function closePopUp(){
     popup.classList.remove("popup--open")
 }
+
+function openMenu(){
+    mobileMenu.classList.add("mobile__menu--open")
+    mobileMenuBurger.style.display = "none"
+}
+
+mobileMenuBurger.addEventListener("click", openMenu)
+closeMobileMenu.addEventListener("click", function(){
+    mobileMenu.classList.remove("mobile__menu--open")
+    mobileMenuBurger.style.display = "block"
+
+})
